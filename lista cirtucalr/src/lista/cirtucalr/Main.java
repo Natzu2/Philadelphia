e380e464d0d99ff4920136bd1f55f1f256907316
Sisti <Sisti@DESKTOP-4P265MQ>
2022-11-13 21:55:54 -0700

    AJAJAJAJJAJAJA EL PENDEJO EL PENDEJO EL PENDFEJO

---
+++ lista cirtucalr/src/lista/cirtucalr/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lista.cirtucalr;
+
+/**
+ *
+ * @author Sisti
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ListaCircular cl = new ListaCircular();  
+        //Adds data to the list  
+        cl.add(1);  
+        cl.add(2);  
+        cl.add(3);  
+        cl.add(4); 
+        cl.add(5);
+        cl.add(6);
+        cl.add(7);
+        cl.add(8);
+        cl.add(9);
+        cl.add(10);
+        cl.add(11);
+        //Displays all the nodes present in the list  
+        cl.display();  
+    }  
+}  
